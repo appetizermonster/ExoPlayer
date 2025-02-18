8f84b4ee9df2703808bab3d933eb4693645a4e6a
Heejin Lee <hjhome2000@naver.com>
2025-02-19 01:35:12 +0900

    Add custom ffmpeg build script

---
+++ extensions/ffmpeg/src/main/jni/build_for_me.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+EXOPLAYER_ROOT="/Users/heejinlee/Workspace/ExoPlayer"
+FFMPEG_EXT_PATH="/Users/heejinlee/Workspace/ExoPlayer/extensions/ffmpeg/src/main"
+NDK_PATH="/Users/heejinlee/Library/Android/sdk/ndk/21.4.7075529"
+HOST_PLATFORM="darwin-x86_64"
+ENABLED_DECODERS=(vorbis opus flac alac)
+
+echo "${FFMPEG_EXT_PATH}" "${NDK_PATH}" "${HOST_PLATFORM}" "${ENABLED_DECODERS[@]}"
+./build_ffmpeg.sh "${FFMPEG_EXT_PATH}" "${NDK_PATH}" "${HOST_PLATFORM}" "${ENABLED_DECODERS[@]}"
